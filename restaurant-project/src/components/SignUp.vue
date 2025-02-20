14f10818b1bce88697a99998376ae32c12165779
Shlomo(שלמה) Hajizadeh <suleyman.hacizade1@gmail.com>
2025-02-20 10:33:26 +0400

    add package.json and initial dependencies for the project

--- restaurant-project/src/components/SignUp.vue
+++ restaurant-project/src/components/SignUp.vue
@@ -2,16 +2,43 @@
     <img class="logo" src="../assets/logo.png" />
     <h1>Sign Up</h1>
     <div class="register">
-        <input type="text" placeholder="Enter Name" />
-        <input type="email" placeholder="Enter Email" />
-        <input type="password" placeholder="Enter Password" />
-        <button>Sign Up</button>
+        <input type="text" v-model="name" placeholder="Enter Name" />
+        <input type="email" v-model="email" placeholder="Enter Email" />
+        <input type="password" v-model="password" placeholder="Enter Password" />
+        <button v-on:click="signUp">Sign Up</button>
     </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
-    name: 'SignUp'
+    name: 'SignUp',
+    data()
+    {
+        return {
+            name: "Shlomo",
+            email: "suleyman.hacizade1@gmail.com",
+            password: "1234567654533455676453",
+        }
+    },
+    methods: {
+       async signUp() 
+        {
+            let result = await axios.post("http://localhost:3000/users", {
+                email: this.email,
+                password: this.password,
+                name: this.name
+            });
+
+            console.log(result);
+            if(result.status == 201)
+            {
+                alert("User created successfully");
+                localStorage.setItem("user-info", JSON.stringify(result.data));
+            }
+        }
+    }
 }
+
 </script>
 
 <style>
