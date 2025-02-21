e8adeaa3fc50c4ff14439edc652f125b97a5865a
Shlomo(שלמה) Hajizadeh <suleyman.hacizade1@gmail.com>
2025-02-21 12:49:24 +0400

    feat: add HeaderSec component for navigation; update HomePage to include HeaderSec and adjust App styling

--- restaurant-project/src/components/HomePage.vue
+++ restaurant-project/src/components/HomePage.vue
@@ -1,9 +1,14 @@
 <template>
+    <HeaderSec></HeaderSec>
     <h1>Hello User, Welcome on Home Page</h1>
 </template>
 <script>
+import HeaderSec from './HeaderSec.vue';
 export default {
     name: 'HomePage',
+    components: {
+        HeaderSec
+    },
     mounted() {
         let user = localStorage.getItem('user-info');
         if (!user) {
